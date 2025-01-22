b6ce63f7227a396138ae9b7a0cbf0297cd6fb19b
Juan Quino <quinojuan@gmail.com>
2025-01-22 16:28:03 -0300

    rename files and madlib

---
+++ madlib.py
@@ -0,0 +1,12 @@
+print("\n")
+print("Welcome to MadLib")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("The story says:")
+print(f"Roses are {color}")
+print(f"{plural_noun} are blue")
+print(f"I love {celebrity}")
+print("The end")
